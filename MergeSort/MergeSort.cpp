0abbae9a0552907626086a369856584c9e3a096b
Ismaeel Mughal <ismaeel.mughal.1912@gmail.com>
2023-01-17 13:08:11 +0500

    Add project files.

---
+++ MergeSort/MergeSort.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+using namespace std;
+
+void printArray(int* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+void merge(int* arrar, const int left, const int mid, const int right) 
+{
+
+}
+
+void mergeSort(int* arr, int begin, int end) {
+	if (begin >= end) 
+	{
+		return;
+	}
+	auto mid = begin + (end - begin) / 2;
+	mergeSort(arr, begin, mid);
+	mergeSort(arr, mid + 1, end);
+	merge(arr, begin, mid, end);
+}
+
+int main() 
+{
+	int arr[] = { 12, 11, 13, 5, 6, 7 };
+	auto arr_size = sizeof(arr) / sizeof(arr[0]);
+
+	cout << "Given array is \n";
+	printArray(arr, arr_size);
+
+	mergeSort(arr, 0, arr_size - 1);
+
+	cout << "\nSorted array is \n";
+	printArray(arr, arr_size);
+	return 0;
+}
